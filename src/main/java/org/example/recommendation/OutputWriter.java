f1c0b742ff650c1f9d13ac3da9aa57c5b7577d90
Ahmed Ashraf <ahmed.ashraf2827@gmail.com>
2025-12-17 09:58:07 +0200

    Refactor: apply formating to OutputWriter file

--- src/main/java/org/example/recommendation/OutputWriter.java
+++ src/main/java/org/example/recommendation/OutputWriter.java
@@ -12,20 +12,23 @@ import java.util.List;
 /**
  * Writes recommendation results to file.
  */
-public class OutputWriter {
+public class OutputWriter
+{
 
     private static final String OUTPUT_FILE = "recommendations.txt";
 
-    public void writeRecommendations(List<UserRecommendations> allRecommendations) throws IOException {
+    public void writeRecommendations(List<UserRecommendations> allRecommendations) throws IOException
+    {
         System.out.println("DEBUG: Writing to: " + new File(OUTPUT_FILE).getAbsolutePath());
         System.out.println("DEBUG: Number of users: " + allRecommendations.size());
 
-        try (FileWriter writer = new FileWriter(OUTPUT_FILE)) {
-            for (int i = 0; i < allRecommendations.size(); i++) {
+        try (FileWriter writer = new FileWriter(OUTPUT_FILE))
+        {
+            for (int i = 0; i < allRecommendations.size(); i++)
+            {
                 writeUserBlock(writer, allRecommendations.get(i));
-                if (i < allRecommendations.size() - 1) {
+                if (i < allRecommendations.size() - 1)
                     writer.write('\n');
-                }
             }
         }
 
@@ -33,17 +36,23 @@ public class OutputWriter {
         openFile();
     }
 
-    public void writeError(String message) {
+    public void writeError(String message)
+    {
         System.err.println("ERROR: " + message);
-        try (FileWriter writer = new FileWriter(OUTPUT_FILE)) {
+        try (FileWriter writer = new FileWriter(OUTPUT_FILE))
+        {
             writer.write("ERROR: " + message);
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             System.err.println("Failed to write error: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private void writeUserBlock(FileWriter writer, UserRecommendations userRec) throws IOException {
+
+    private void writeUserBlock(FileWriter writer, UserRecommendations userRec) throws IOException
+    {
         User user = userRec.getUser();
         System.out.println("DEBUG: Writing recommendations for: " + user.getUserName());
 
@@ -53,23 +62,27 @@ public class OutputWriter {
         writeMovieList(writer, userRec.getRecommendations());
     }
 
-    private void writeMovieList(FileWriter writer, List<Movie> movies) throws IOException {
+    private void writeMovieList(FileWriter writer, List<Movie> movies) throws IOException
+    {
         System.out.println("DEBUG: Number of recommendations: " + movies.size());
 
-        for (int i = 0; i < movies.size(); i++) {
+        for (int i = 0; i < movies.size(); i++)
+        {
             writer.write(movies.get(i).getTitle());
-            if (i < movies.size() - 1) {
+            if (i < movies.size() - 1)
                 writer.write(", ");
-            }
         }
     }
 
-    private void openFile() {
-        try {
-            if (Desktop.isDesktopSupported()) {
+    private void openFile()
+    {
+        try
+        {
+            if (Desktop.isDesktopSupported())
                 Desktop.getDesktop().open(new File(OUTPUT_FILE));
-            }
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             System.err.println("Could not open file: " + e.getMessage());
         }
     }
